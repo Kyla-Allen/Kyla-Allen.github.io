29ee0c907eacaa64a8eb0a9b7cf32f9d8e4785ec
Kyla Allen <themightyflorp@Kylas-MacBook-Air.local>
2018-12-04 11:49:21 -0600

    implemented bootstrap and major theme change

--- js/main_page.js
+++ js/main_page.js
@@ -1,14 +1,7 @@
 (function () {
     "use strict";
 
-// Snagging HTML things
-    var mobileImg = document.getElementById("mobile_img");
-    var mobileItems = document.getElementById("mobile_items");
 
-    mobileImg.addEventListener("click", function (ev) {
-        console.log("Test");
-        // mobileItems.toggle("showOff");
-    });
 
 
 
